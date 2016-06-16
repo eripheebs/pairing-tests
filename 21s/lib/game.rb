9700bb0af611852456935eed118f57b62d3b0461
eripheebs <erikapheby@gmail.com>
2016-06-16 18:09:23 +0100

    refactoring

--- 21s/lib/game.rb
+++ 21s/lib/game.rb
@@ -21,14 +21,17 @@ class Game
   end
 
   def determine_winner
-    return player1 if blackjack?(player1)
-    return player2 if blackjack?(player2)
+    game_isnt_over_error
     return "nobody, it's a draw" if draw?(player1, player2)
-    return player1 if higher?(player1, player2) && lower_than_blackjack(player1)
-    return player2
+    return higher_score(player1, player2)
   end
 
   private
+
+  def higher_score(player1, player2)
+    return player1 if higher?(player1, player2) && lower_or_eq_blackjack(player1)
+    return player2
+  end
 
   def play_round(player)
     until over_17(player) do
@@ -36,8 +39,8 @@ class Game
     end
   end
 
-  def lower_than_blackjack(player)
-    score_hand(player) < BLACKJACK
+  def lower_or_eq_blackjack(player)
+    score_hand(player) <= BLACKJACK
   end
 
   def higher?(player, player2)
@@ -48,8 +51,12 @@ class Game
     score_hand(player) == score_hand(player2)
   end
 
-  def game_over?
+  def game_over_21?
     over_blackjack(player1) || over_blackjack(player2)
+  end
+
+  def game_over?
+    over_17(player1) || over_17(player2)
   end
 
   def over_17(player)
@@ -69,6 +76,10 @@ class Game
   end
 
   def game_over_error(score)
-    raise "Game is over" if game_over?
+    raise "Game is over" if game_over_21?
+  end
+
+  def game_isnt_over_error
+    raise "Game isn't over" if !game_over?
   end
 end
