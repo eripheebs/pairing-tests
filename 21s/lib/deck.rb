2b7fa46981eed48dfb5de0cface358d8a2137c5b
eripheebs <erikapheby@gmail.com>
2016-06-16 16:32:43 +0100

    fixed deck of cards

--- 21s/lib/deck.rb
+++ 21s/lib/deck.rb
@@ -1,5 +1,5 @@
 class Deck
-  PACK_OF_CARDS = [*2..10, *2..10, *2..10, *2..10]
+  PACK_OF_CARDS = ([*2..10, *2..10, *2..10, *2..10] << [10]*12 << [11]*4).flatten
 
   def initialize
     @cards = PACK_OF_CARDS
